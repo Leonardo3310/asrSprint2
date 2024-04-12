b1ac777e88fc33df886a6df9bab7c19794787413
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-04-11 22:31:55 -0500

    x

--- asrSprint2/urls.py
+++ asrSprint2/urls.py
@@ -1,7 +1,7 @@
-"""asrSprint2 URL Configuration
+"""monitoring URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('documents/', include('documents.urls')),
-    # otras rutas
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.index),
+    path('', include('measurements.urls')),
+    path('', include('variables.urls')),
+    path('health-check/', views.healthCheck),
+]
