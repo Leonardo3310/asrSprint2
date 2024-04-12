f5ef35b11ae5d0c210fc0615b7cf740f83e6f7be
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-04-11 20:55:19 -0500

    x

--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# documents/models.py
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+# documents/views.py
+
+from .models import Document
+from .forms import DocumentForm
+
+def document_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'documents/upload_complete.html', {'form': form})
+    else:
+        form = DocumentForm()
+    return render(request, 'documents/document_upload.html', {'form': form})
+
+def document_list(request):
+    documents = Document.objects.all()
+    return render(request, 'documents/document_list.html', {'documents': documents})
